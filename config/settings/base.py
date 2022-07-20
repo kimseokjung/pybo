b095a00c4cf13096fe44fd7d260abdaa74f00523
김석중 <slsms311@gmail.com>
2022-07-20 15:38:35 +0900

    LOGGING

--- config/settings/base.py
+++ config/settings/base.py
@@ -162,7 +162,7 @@ LOGGING = {
         },
         'file': {
             'level': 'INFO',
-            'filters': ['require_debug_false'],
+            'filters': ['spacial'],
             'class': 'logging.handler.RotatingFileHandler',
             'filename': BASE_DIR / 'logs/mysite.log',
             'maxBytes': 1024*1024*5,    # 5MB
